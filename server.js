2138c17980d7d306d4a8a67a6195bd362e9f6ab5
Subhasish Shit <subhashit4126@gmail.com>
2024-06-25 12:13:20 +0530

    Doctor Booking Website

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const express= require("express");
-// const app=express();
-// require('dotenv').config()
-// const dbConfig=require("./config/dbConfig");
-
-// app.use(express.json());
-// const userRoute=require("./routes/userRoute");
-// app.use('/api/user', userRoute);
-// const port = process.env.PORT || 5000;
-// //console.log(process.env.MONGO_URL);
-
-// app.listen(port, () => console.log(`Node server started at port ${port}`));
-
 const express = require("express");
 const cors = require("cors"); // Import the cors package
 const app = express();
@@ -18,6 +5,7 @@ require('dotenv').config();
 const dbConfig = require("./config/dbConfig");
 
 app.use(express.json());
+const path = require("path");
 
 // Use the CORS middleware
 app.use(cors({
@@ -32,6 +20,13 @@ app.use("/api/admin", adminRoute);
 
 const doctorRoute = require("./routes/doctorsRoute");
 app.use("/api/doctor", doctorRoute);
+
+app.use(express.static(path.join(__dirname, "./client/build")))
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node server started at port ${port}`));
